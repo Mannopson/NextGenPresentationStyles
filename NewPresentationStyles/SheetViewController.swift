94a2c438c890646259c89be7be6741de59f19d8f
Mannopson <mannopson@yahoo.com>
2021-06-26 18:46:24 +0500

    Added a new style of sheets

---
+++ NewPresentationStyles/SheetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SheetViewController.swift
+//  NewPresentationStyles
+//
+//  Created by AzizOfficial on 6/25/21.
+//
+
+import UIKit
+
+class SheetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NewPresentationStyles/ViewController.swift
+++ NewPresentationStyles/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private func menu() -> UIMenu {
+        let medium = UIAction.init(title: "Medium") { action in
+            guard let navigationController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "NAVIGATION_CONTROLLER") as? UINavigationController else { return }
+            navigationController.modalPresentationStyle = .popover
+            if let popover = navigationController.popoverPresentationController {
+                let sheet = popover.adaptiveSheetPresentationController
+                sheet.detents = [.medium()]
+                sheet.prefersGrabberVisible = true
+            }
+            self.present(navigationController, animated: true, completion: nil)
+        }
+        let large = UIAction.init(title: "Large") { action in
+            guard let navigationController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "NAVIGATION_CONTROLLER") as? UINavigationController else { return }
+            navigationController.modalPresentationStyle = .popover
+            if let popover = navigationController.popoverPresentationController {
+                let sheet = popover.adaptiveSheetPresentationController
+                sheet.detents = [.large()]
+                sheet.prefersGrabberVisible = true
+            }
+            self.present(navigationController, animated: true, completion: nil)
+        }
+        let both = UIAction.init(title: "Both") { action in
+            guard let navigationController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "NAVIGATION_CONTROLLER") as? UINavigationController else { return }
+            navigationController.modalPresentationStyle = .popover
+            if let popover = navigationController.popoverPresentationController {
+                let sheet = popover.adaptiveSheetPresentationController
+                sheet.detents = [.medium(), .large()]
+                sheet.prefersGrabberVisible = true
+            }
+            self.present(navigationController, animated: true, completion: nil)
+        }
+        return UIMenu.init(options: .displayInline, children: [medium, large, both])
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem.init(systemItem: .add, primaryAction: nil, menu: menu())
     }
-
-
 }
 
